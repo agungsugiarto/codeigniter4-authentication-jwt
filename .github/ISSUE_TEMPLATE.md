2745635e9fa44add65a779a07b746a0f0bf5328b
agungsugiarto <me.agungsugiarto@gmail.com>
2022-03-29 00:02:06 +0700

    Fix Call to undefined method Builder::withClaim()

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,20 @@
+### Subject of the issue
+Describe your issue here.
+
+### Your environment
+| Q                 | A
+| ----------------- | ---
+| Bug?              | no / yes
+| New Feature?      | no / yes
+| Framework version | 4.x.y
+| Package version   | 1.x.y
+| PHP version       | 5.x.y / 7.x.y
+
+### Steps to reproduce
+Tell us how to reproduce this issue.
+
+### Expected behaviour
+Tell us what should happen
+
+### Actual behaviour
+Tell us what happens instead
--- tests/Providers/JWT/LcobucciTest.php
+++ tests/Providers/JWT/LcobucciTest.php
@@ -48,134 +48,142 @@ class LcobucciTest extends AbstractTestCase
     }
 
     /** @test */
-    // public function it_should_return_the_token_when_passing_a_valid_payload_to_encode()
-    // {
-    //     $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp + 3600, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
+    public function it_should_return_the_token_when_passing_a_valid_payload_to_encode()
+    {
+        $this->markTestSkipped("[HELP] Fix me...!");
 
-    //     $this->builder->shouldReceive('unsign')->once()->andReturnSelf();
-    //     $this->builder->shouldReceive('set')->times(count($payload));
-    //     $this->builder->shouldReceive('sign')->once()->with(Mockery::any(), 'secret');
-    //     $this->builder->shouldReceive('getToken')->once()->andReturn('foo.bar.baz');
+        $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp + 3600, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
 
-    //     $token = $this->getProvider('secret', 'HS256')->encode($payload);
+        $this->builder->shouldReceive('unsign')->once()->andReturnSelf();
+        $this->builder->shouldReceive('set')->times(count($payload));
+        $this->builder->shouldReceive('sign')->once()->with(Mockery::any(), 'secret');
+        $this->builder->shouldReceive('getToken')->once()->andReturn('foo.bar.baz');
 
-    //     $this->assertSame('foo.bar.baz', $token);
-    // }
+        $token = $this->getProvider('secret', 'HS256')->encode($payload);
 
-    // /** @test */
-    // public function it_should_throw_an_invalid_exception_when_the_payload_could_not_be_encoded()
-    // {
-    //     $this->expectException(JWTException::class);
-    //     $this->expectExceptionMessage('Could not create token:');
+        $this->assertSame('foo.bar.baz', $token);
+    }
 
-    //     $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
+    /** @test */
+    public function it_should_throw_an_invalid_exception_when_the_payload_could_not_be_encoded()
+    {
+        $this->markTestSkipped("[HELP] Fix me...!");
 
-    //     $this->builder->shouldReceive('unsign')->once()->andReturnSelf();
-    //     $this->builder->shouldReceive('set')->times(count($payload));
-    //     $this->builder->shouldReceive('sign')->once()->with(Mockery::any(), 'secret')->andThrow(new Exception);
+        $this->expectException(JWTException::class);
+        $this->expectExceptionMessage('Could not create token:');
 
-    //     $this->getProvider('secret', 'HS256')->encode($payload);
-    // }
+        $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
 
-    // /** @test */
-    // public function it_should_return_the_payload_when_passing_a_valid_token_to_decode()
-    // {
-    //     $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp + 3600, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
+        $this->builder->shouldReceive('unsign')->once()->andReturnSelf();
+        $this->builder->shouldReceive('set')->times(count($payload));
+        $this->builder->shouldReceive('sign')->once()->with(Mockery::any(), 'secret')->andThrow(new Exception);
 
-    //     $this->parser->shouldReceive('parse')->once()->with('foo.bar.baz')->andReturn(Mockery::self());
-    //     $this->parser->shouldReceive('verify')->once()->with(Mockery::any(), 'secret')->andReturn(true);
-    //     $this->parser->shouldReceive('getClaims')->once()->andReturn($payload);
+        $this->getProvider('secret', 'HS256')->encode($payload);
+    }
 
-    //     $this->assertSame($payload, $this->getProvider('secret', 'HS256')->decode('foo.bar.baz'));
-    // }
+    /** @test */
+    public function it_should_return_the_payload_when_passing_a_valid_token_to_decode()
+    {
+        $this->markTestSkipped("[HELP] Fix me...!");
 
-    // /** @test */
-    // public function it_should_throw_a_token_invalid_exception_when_the_token_could_not_be_decoded_due_to_a_bad_signature()
-    // {
-    //     $this->expectException(TokenInvalidException::class);
-    //     $this->expectExceptionMessage('Token Signature could not be verified.');
+        $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp + 3600, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
 
-    //     $this->parser->shouldReceive('parse')->once()->with('foo.bar.baz')->andReturn(Mockery::self());
-    //     $this->parser->shouldReceive('verify')->once()->with(Mockery::any(), 'secret')->andReturn(false);
-    //     $this->parser->shouldReceive('getClaims')->never();
+        $this->parser->shouldReceive('parse')->once()->with('foo.bar.baz')->andReturn(Mockery::self());
+        $this->parser->shouldReceive('verify')->once()->with(Mockery::any(), 'secret')->andReturn(true);
+        $this->parser->shouldReceive('getClaims')->once()->andReturn($payload);
 
-    //     $this->getProvider('secret', 'HS256')->decode('foo.bar.baz');
-    // }
+        $this->assertSame($payload, $this->getProvider('secret', 'HS256')->decode('foo.bar.baz'));
+    }
 
-    // /** @test */
-    // public function it_should_throw_a_token_invalid_exception_when_the_token_could_not_be_decoded()
-    // {
-    //     $this->expectException(TokenInvalidException::class);
-    //     $this->expectExceptionMessage('Could not decode token:');
+    /** @test */
+    public function it_should_throw_a_token_invalid_exception_when_the_token_could_not_be_decoded_due_to_a_bad_signature()
+    {
+        $this->markTestSkipped("[HELP] Fix me...!");
 
-    //     $this->parser->shouldReceive('parse')->once()->with('foo.bar.baz')->andThrow(new InvalidArgumentException);
-    //     $this->parser->shouldReceive('verify')->never();
-    //     $this->parser->shouldReceive('getClaims')->never();
+        $this->expectException(TokenInvalidException::class);
+        $this->expectExceptionMessage('Token Signature could not be verified.');
 
-    //     $this->getProvider('secret', 'HS256')->decode('foo.bar.baz');
-    // }
+        $this->parser->shouldReceive('parse')->once()->with('foo.bar.baz')->andReturn(Mockery::self());
+        $this->parser->shouldReceive('verify')->once()->with(Mockery::any(), 'secret')->andReturn(false);
+        $this->parser->shouldReceive('getClaims')->never();
 
-    // /** @test */
-    // public function it_should_generate_a_token_when_using_an_rsa_algorithm()
-    // {
-    //     $provider = $this->getProvider(
-    //         'does_not_matter',
-    //         'RS256',
-    //         ['private' => $this->getDummyPrivateKey(), 'public' => $this->getDummyPublicKey()]
-    //     );
+        $this->getProvider('secret', 'HS256')->decode('foo.bar.baz');
+    }
 
-    //     $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp + 3600, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
+    /** @test */
+    public function it_should_throw_a_token_invalid_exception_when_the_token_could_not_be_decoded()
+    {
+        $this->expectException(TokenInvalidException::class);
+        $this->expectExceptionMessage('Could not decode token:');
 
-    //     $this->builder->shouldReceive('unsign')->once()->andReturnSelf();
-    //     $this->builder->shouldReceive('set')->times(count($payload));
-    //     $this->builder->shouldReceive('sign')->once()->with(Mockery::any(), Mockery::type(Key::class));
-    //     $this->builder->shouldReceive('getToken')->once()->andReturn('foo.bar.baz');
+        $this->parser->shouldReceive('parse')->once()->with('foo.bar.baz')->andThrow(new InvalidArgumentException);
+        $this->parser->shouldReceive('verify')->never();
+        $this->parser->shouldReceive('getClaims')->never();
 
-    //     $token = $provider->encode($payload);
+        $this->getProvider('secret', 'HS256')->decode('foo.bar.baz');
+    }
 
-    //     $this->assertSame('foo.bar.baz', $token);
-    // }
+    /** @test */
+    public function it_should_generate_a_token_when_using_an_rsa_algorithm()
+    {
+        $provider = $this->getProvider(
+            'does_not_matter',
+            'RS256',
+            ['private' => $this->getDummyPrivateKey(), 'public' => $this->getDummyPublicKey()]
+        );
 
-    // /** @test */
-    // public function it_should_throw_a_exception_when_the_algorithm_passed_is_invalid()
-    // {
-    //     $this->expectException(JWTException::class);
-    //     $this->expectExceptionMessage('The given algorithm could not be found');
+        $payload = ['sub' => 1, 'exp' => $this->testNowTimestamp + 3600, 'iat' => $this->testNowTimestamp, 'iss' => '/foo'];
 
-    //     $this->parser->shouldReceive('parse')->never();
-    //     $this->parser->shouldReceive('verify')->never();
+        $this->builder->shouldReceive('unsign')->once()->andReturnSelf();
+        $this->builder->shouldReceive('set')->times(count($payload));
+        $this->builder->shouldReceive('sign')->once()->with(Mockery::any(), Mockery::type(Key::class));
+        $this->builder->shouldReceive('getToken')->once()->andReturn('foo.bar.baz');
 
-    //     $this->getProvider('secret', 'AlgorithmWrong')->decode('foo.bar.baz');
-    // }
+        $token = $provider->encode($payload);
 
-    // /** @test */
-    // public function it_should_return_the_public_key()
-    // {
-    //     $provider = $this->getProvider(
-    //         'does_not_matter',
-    //         'RS256',
-    //         $keys = ['private' => $this->getDummyPrivateKey(), 'public' => $this->getDummyPublicKey()]
-    //     );
+        $this->assertSame('foo.bar.baz', $token);
+    }
 
-    //     $this->assertSame($keys['public'], $provider->getPublicKey());
-    // }
+    /** @test */
+    public function it_should_throw_a_exception_when_the_algorithm_passed_is_invalid()
+    {
+        $this->expectException(JWTException::class);
+        $this->expectExceptionMessage('The given algorithm could not be found');
 
-    // /** @test */
-    // public function it_should_return_the_keys()
-    // {
-    //     $provider = $this->getProvider(
-    //         'does_not_matter',
-    //         'RS256',
-    //         $keys = ['private' => $this->getDummyPrivateKey(), 'public' => $this->getDummyPublicKey()]
-    //     );
+        $this->parser->shouldReceive('parse')->never();
+        $this->parser->shouldReceive('verify')->never();
 
-    //     $this->assertSame($keys, $provider->getKeys());
-    // }
+        $this->getProvider('secret', 'AlgorithmWrong')->decode('foo.bar.baz');
+    }
 
-    // public function getProvider($secret, $algo, array $keys = [])
-    // {
-    //     return new Lcobucci($this->builder, $this->parser, $secret, $algo, $keys);
-    // }
+    /** @test */
+    public function it_should_return_the_public_key()
+    {
+        $provider = $this->getProvider(
+            'does_not_matter',
+            'RS256',
+            $keys = ['private' => $this->getDummyPrivateKey(), 'public' => $this->getDummyPublicKey()]
+        );
+
+        $this->assertSame($keys['public'], $provider->getPublicKey());
+    }
+
+    /** @test */
+    public function it_should_return_the_keys()
+    {
+        $provider = $this->getProvider(
+            'does_not_matter',
+            'RS256',
+            $keys = ['private' => $this->getDummyPrivateKey(), 'public' => $this->getDummyPublicKey()]
+        );
+
+        $this->assertSame($keys, $provider->getKeys());
+    }
+
+    public function getProvider($secret, $algo, array $keys = [])
+    {
+        return new Lcobucci($this->builder, $this->parser, $secret, $algo, $keys);
+    }
 
     public function getDummyPrivateKey()
     {
